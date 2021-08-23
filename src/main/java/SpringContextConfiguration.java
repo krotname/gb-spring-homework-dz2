04c566ddabf5a0bb60d2c8a58d4802e4e2cc97a0
Kreout <ia@krot.name>
2021-08-23 20:10:14 +0300

    fix

--- src/main/java/SpringContextConfiguration.java
+++ src/main/java/SpringContextConfiguration.java
@@ -10,7 +10,7 @@ public class SpringContextConfiguration {
         return new Cart();
     }
 
-    
+
     @Bean
     public ProductRepository productRepository() {
         return new ProductRepository();
